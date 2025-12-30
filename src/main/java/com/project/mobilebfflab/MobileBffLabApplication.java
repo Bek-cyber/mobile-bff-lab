f478ce914b5b567530212c826f0e23a4ad4f5558
Ulugbek Ishmatov <ishmatovulugbek@gmail.com>
2025-12-30 21:46:42 +0300

    Fix .gitignore (clean Gradle + IDE setup) + Initial Spring Boot BFF skeleton

---
+++ src/main/java/com/project/mobilebfflab/MobileBffLabApplication.java
@@ -0,0 +1,13 @@
+package com.project.mobilebfflab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileBffLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileBffLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/mobilebfflab/MobileBffLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mobilebfflab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileBffLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
